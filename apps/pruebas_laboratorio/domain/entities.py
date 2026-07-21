dba4b8a152517c8840f8d23e20299e633e0b1c07
Jafet Poco Chire <jpococ@unsa.edu.pe>
2026-07-21 16:23:15 -0500

    agregando entidad solicitud prueba y resultados prueba

--- apps/pruebas_laboratorio/domain/entities.py
+++ apps/pruebas_laboratorio/domain/entities.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass, field
 from datetime import date, datetime
 from decimal import Decimal
 from typing import Optional
-from apps.mantenimiento_biomedico.domain.enums import ()
 
 @dataclass
 class Paciente:
@@ -13,4 +12,21 @@ class Paciente:
     sexo: str = ""
     direccion: str = ""
     telefono: str = ""
-    email: str = ""
+
+@dataclass
+class SolicitudPrueba:
+    id: Optional[int] = None
+    paciente: Optional[Paciente] = None
+    paciente_id: Optional[int] = None
+    tipo_prueba: str = ""
+    fecha_solicitud: datetime = field(default_factory=datetime.now)
+    estado: str = "Pendiente"
+
+@dataclass
+class ResultadoPrueba:
+    id: Optional[int] = None
+    solicitud: Optional[SolicitudPrueba] = None
+    solicitud_id: Optional[int] = None
+    resultado: str = ""
+    fecha_resultado: datetime = field(default_factory=datetime.now)
+
