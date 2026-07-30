77102cdaeefb2e65789264192fad35811a64366c
Berly Dueñas <bduenasm@unsa.edu.pe>
2026-07-30 07:16:33 -0500

    Quitar secret key de django

--- HospitalDjangoAPI/settings.py
+++ HospitalDjangoAPI/settings.py
@@ -12,15 +12,17 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import posixpath
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e7afe21b-46f0-4d3c-a297-69d94d0de1be'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
