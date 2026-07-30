02594a670db360d37fef7ab265b8cd3524430d8d
Berly Dueñas <bduenasm@unsa.edu.pe>
2026-07-30 00:18:16 -0500

    agregado swagger para reportes de mantenimiento #15

--- HospitalDjangoAPI/settings.py
+++ HospitalDjangoAPI/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'apps.admision_hospitalizacion',
     'apps.mantenimiento_biomedico',
     'apps.consultorio_externo',
